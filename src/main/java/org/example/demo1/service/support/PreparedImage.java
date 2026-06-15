3048f7c19f6f4e65975c8a0113a5b079d39cde9d
chenmilin <czqCZQ197623@>
2026-06-15 21:01:39 +0800

    refactor package structure and remove old logging classes

--- src/main/java/org/example/demo1/service/support/PreparedImage.java
+++
@@ -1,9 +0,0 @@
-package org.example.demo1.service.support;
-
-public record PreparedImage(
-        String contentType,
-        long sizeBytes,
-        byte[] bytes,
-        String dataUri
-) {
-}
--- src/main/java/org/example/demo1/service/support/ThrowingSupplier.java
+++
@@ -1,7 +0,0 @@
-package org.example.demo1.service.support;
-
-@FunctionalInterface
-public interface ThrowingSupplier<T> {
-
-    T get() throws Exception;
-}
